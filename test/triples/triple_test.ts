6e8e1e755f088495391595aff57e717dcb40ce1e
Eyas Sharaiha <eyas@google.com>
2020-01-14 15:24:42 -0500

    Add coverage for "Triple" formatting.

---
+++ test/triples/triple_test.ts
@@ -0,0 +1,62 @@
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {expect} from 'chai';
+
+import {Format, ObjectPredicate, Triple} from '../../src/triples/triple';
+import {Rdfs, SchemaString, UrlNode} from '../../src/triples/types';
+
+describe('Format(Triple)', () => {
+  it('smoke test 1', () => {
+    const triple: Triple = {
+      Subject: UrlNode.Parse('https://schema.org/Person'),
+      Predicate: UrlNode.Parse('https://schema.org/knowsAbout'),
+      Object: SchemaString.Parse('"food"')!
+    };
+
+    expect(Format(triple)).to.equal('(Person, knowsAbout, "food")');
+  });
+
+  it('smoke test 2', () => {
+    const triple: Triple = {
+      Subject: UrlNode.Parse('https://schema.org/Organization'),
+      Predicate: UrlNode.Parse('https://schema.org/knowsAbout'),
+      Object: Rdfs.Parse('rdfs:Class')!
+    };
+
+    expect(Format(triple)).to.equal('(Organization, knowsAbout, rdfs:Class)');
+  });
+});
+
+describe('Format(ObjectPredicate)', () => {
+  it('smoke test 1', () => {
+    const triple: ObjectPredicate = {
+      Predicate: UrlNode.Parse('https://schema.org/knowsAbout'),
+      Object: SchemaString.Parse('"food"')!
+    };
+
+    expect(Format(triple)).to.equal('(knowsAbout, "food")');
+  });
+
+  it('smoke test 2', () => {
+    const triple: ObjectPredicate = {
+      Predicate: UrlNode.Parse('https://schema.org/knowsAbout'),
+      Object: Rdfs.Parse('rdfs:Class')!
+    };
+
+    expect(Format(triple)).to.equal('(knowsAbout, rdfs:Class)');
+  });
+});
