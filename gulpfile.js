972aca98695c63012777fb3dde49584593bc56f7
Eyas Sharaiha <eyas@google.com>
2020-01-15 12:25:49 -0500

    Fold most uses of gulp into simple npm scripts.
    There's a bunch of gulp logic that's realy just straightforward in bash.
    So we move it to npm scripts. Removing the need to have gulp installed
    globally, and removing the need for gulp-typescript, which had its host
    of issues and incompatibilities with tsc --b and some of the new
    package updates we introduced.

--- gulpfile.js
+++ gulpfile.js
@@ -15,32 +15,7 @@
  */
 
 const gulp = require('gulp');
-const tsc = require('gulp-typescript');
-const file = require('gulp-file');
 const del = require('del');
-
-const cpSpwan = require('child_process').spawn;
-const through2 = require('through2');
-function spawn(command, args = [], options = {}) {
-  if (!options.env) {
-    options.env = process.env;
-  }
-  if (!options.cwd) {
-    options.cwd = process.cwd();
-  }
-  return through2.obj(function(file, _, flush) {
-    const cp = cpSpwan(command, args, options);
-    cp.stderr.on(
-        'data', err => console.error(Buffer.from(err).toString('utf-8')));
-    cp.on('error', err => console.log(err));
-
-    file.contents = cp.stdout;
-    this.push(file);
-    cp.on('close', () => {
-      flush();
-    });
-  });
-}
 
 gulp.task('clean', () => del([
                      'built',
@@ -49,35 +24,3 @@ gulp.task('clean', () => del([
                      '!dist/schema/package.json',
                      '!dist/schema/README.md',
                    ]));
-
-const nodeProject = tsc.createProject('src/tsconfig.json');
-gulp.task('build-generator', () => {
-  return nodeProject.src().pipe(nodeProject()).pipe(gulp.dest('built/src'));
-});
-
-gulp.task('generate-ts', gulp.series('build-generator', () => {
-  return file('schema.ts', '', {src: true})
-      .pipe(spawn('node', ['built/src/cli/cli.js']))
-      .pipe(gulp.dest('built/ts-schema'));
-}));
-
-gulp.task(
-    'prepare-package',
-    () => gulp.src(['LICENSE']).pipe(gulp.dest('dist/schema')));
-
-gulp.task(
-    'generate-package',
-    gulp.series(
-        'clean',
-        gulp.parallel('generate-ts', 'prepare-package'),
-        gulp.parallel(
-            () => {
-              return gulp.src('built/ts-schema/*.ts')
-                  .pipe(tsc({
-                    noImplicitAny: true,
-                    declaration: true,
-                  }))
-                  .pipe(gulp.dest('dist/schema'));
-            },
-            () => gulp.src('built/src/**/*').pipe(gulp.dest('dist/gen'))),
-        ));
