5c834d1778633a9ad30cdcc8d4971be401431d39
Quetzal Rivera <QuetzalDeveloper@outlook.com>
2025-05-19 12:35:42 -0600

    chore: update tests

--- packages/schema-dts-gen/test/baselines/data_type_union_test.ts
+++ packages/schema-dts-gen/test/baselines/data_type_union_test.ts
@@ -43,36 +43,44 @@ test(`baseline_${basename(import.meta.url)}`, async () => {
   );
 
   expect(actual).toMatchInlineSnapshot(`
-    "/** Used at the top-level node to indicate the context for the JSON-LD objects used. The context provided in this type is compatible with the keys and URLs in the rest of this generated file. */
-    export type WithContext<T extends Thing> = T & {
-        "@context": "https://schema.org";
-    };
-    export interface Graph {
-        "@context": "https://schema.org";
-        "@graph": readonly Thing[];
-    }
-    type SchemaValue<T> = T | readonly T[];
-    type IdReference = {
-        /** IRI identifying the canonical address of this object. */
-        "@id": string;
-    };
+"/** Used at the top-level node to indicate the context for the JSON-LD objects used. The context provided in this type is compatible with the keys and URLs in the rest of this generated file. */
+export type WithContext<T extends Thing> = T & {
+    "@context": "https://schema.org";
+};
+export interface Graph {
+    "@context": "https://schema.org";
+    "@graph": readonly Thing[];
+}
+type SchemaValue<T> = T | readonly T[];
+type IdReference = {
+    /** IRI identifying the canonical address of this object. */
+    "@id": string;
+};
+type InputActionConstraints<T extends ActionBase> = Partial<{
+    [K in Exclude<keyof T, \`@\${string}\`> as \`\${string & K}-input\`]: PropertyValueSpecification | string;
+}>;
+type OutputActionConstraints<T extends ActionBase> = Partial<{
+    [K in Exclude<keyof T, \`@\${string}\`> as \`\${string & K}-output\`]: PropertyValueSpecification | string;
+}>;
+/** Provides input and output action constraints for an action. */
+export type WithActionConstraints<T extends ActionBase> = T & InputActionConstraints<T> & OutputActionConstraints<T>;
 
-    export type Number = number | \`\${number}\`;
+export type Number = number | \`\${number}\`;
 
-    export type Text = string;
+export type Text = string;
 
-    /** The basic data types such as Integers, Strings, etc. */
-    export type DataType = Number | Text;
+/** The basic data types such as Integers, Strings, etc. */
+export type DataType = Number | Text;
 
-    interface ThingBase extends Partial<IdReference> {
-        "age"?: SchemaValue<Number>;
-        "name"?: SchemaValue<Text>;
-    }
-    interface ThingLeaf extends ThingBase {
-        "@type": "Thing";
-    }
-    export type Thing = ThingLeaf;
+interface ThingBase extends Partial<IdReference> {
+    "age"?: SchemaValue<Number>;
+    "name"?: SchemaValue<Text>;
+}
+interface ThingLeaf extends ThingBase {
+    "@type": "Thing";
+}
+export type Thing = ThingLeaf;
 
-    "
-  `);
+"
+`);
 });
